550d151ea1b7cdebc63861541e2a5d5500853540
MIKEYBOYPC\edree <michael.endrizzi@gmail.com>
2023-11-04 14:10:13 +0100

    js works with test

--- other/af-plans/juiceshop-selenium-auth/js-test.sh
+++ other/af-plans/juiceshop-selenium-auth/js-test.sh
@@ -1,12 +1,17 @@
 # Ensure that ZAP is set to the full path of the zap.sh script installed
 
 # Standard macOS location
-# ZAP=/Applications/OWASP\ ZAP.app/Contents/Java/zap.sh
+# ZAP=/Applications/OWASP/ ZAP.app/Contents/Java/zap.sh
 
-ZAP=zap.sh
-
+ZAP='/mnt/c/Program Files/ZAP/Zed Attack Proxy/zap.sh'
+APPDIR='/mnt/c/Users/edree/ZAP/community-scripts/other/af-plans/juiceshop-selenium-auth/'
+export SessFile=$APPDIR"JuiceShopSession.js"
+export AuthFile=$APPDIR"JuiceShopAuthentication.js"
+echo $SessFile
+echo $AuthFile
 export JS_USER=test@test.com
 export JS_PWD=test123
 
-$ZAP -cmd -autorun /full/path/juiceshop-test.yaml
+cd $APPDIR
+"$ZAP" -cmd -autorun $APPDIR"juiceshop-test.yaml"
 
